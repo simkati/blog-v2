c02f7cbaddb263dc6e6fc3d83144677e33ee3229
ksim <ksimkati@gmail.com>
2024-02-15 01:26:49 +0100

    image upload

---
+++ pages/api/image.ts
@@ -0,0 +1,36 @@
+import { NextApiHandler } from "next";
+import formidable from "formidable";
+import cloudunary from "../../lib/cloudinary";
+import cloudinary from "../../lib/cloudinary";
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      return uploadNewImage(req, res);
+    case "GET":
+      return readAllImage(req, res);
+    default:
+      return res.status(404).send("Not found!");
+  }
+};
+const uploadNewImage: NextApiHandler = (req, res) => {
+  const form = formidable();
+  form.parse(req, async (err, fields, files) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (!files.image) return res.status(500).json({ error: err.message });
+    const imageFile = files.image[0];
+    await cloudinary.uploader.upload(imageFile.filepath, {
+      folder: "dev-blogs",
+    });
+  });
+};
+
+const readAllImage: NextApiHandler = (req, res) => {};
+
+export default handler;
